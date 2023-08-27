ddc3979631bc9056bff228e89acb0c7dbda840a4
Mihir Gupta <mihirgupta0900@gmail.com>
2023-08-27 13:29:11 +0530

    feat(api): only use zod for env validation

--- apps/api/src/env.ts
+++ apps/api/src/env.ts
@@ -1,20 +1,28 @@
-import { createEnv } from "@t3-oss/env-core";
 import * as dotenv from "dotenv";
 import { z } from "zod";
 
 dotenv.config();
 
-export const env = createEnv({
-  clientPrefix: "",
-  client: {},
-  server: {
-    TODOIST_TOKEN: z.string().length(40),
-    TODOIST_PROJECT: z.string().min(1),
-    GITHUB_TARGET_REVIEWER: z.string().min(1),
-  },
-  /**
-   * What object holds the environment variables at runtime.
-   * Often `process.env` or `import.meta.env`
-   */
-  runtimeEnv: process.env,
+const envSchema = z.object({
+  TODOIST_TOKEN: z.string().length(40),
+  TODOIST_PROJECT: z.string().min(1),
+  GITHUB_TARGET_REVIEWER: z.string().min(1),
 });
+
+const parsedEnv = envSchema.safeParse(process.env);
+
+if (parsedEnv.success === false) {
+  console.error("❌ Invalid environment variables:");
+
+  const errorsFlattened = parsedEnv.error.flatten();
+  if (errorsFlattened.formErrors.length > 0) {
+    console.error(errorsFlattened.formErrors[0]);
+  }
+  if (Object.values(errorsFlattened.fieldErrors).length > 0) {
+    console.error(errorsFlattened.fieldErrors);
+  }
+
+  throw new Error("Invalid environment variables");
+}
+
+export const env = parsedEnv.data;
